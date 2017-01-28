863b61d9166663ddaa316012e84a35b7c6274a4a
JarUrb <JarUrb@users.noreply.github.com>
2017-01-29 00:38:39 +0100

    Don't create dummy sensor

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
 
-import datetime
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-from models import Base, Sensor
+from models import Base
 from settings import DATABASE_URI
 
 
 def setup():
     engine = create_engine(DATABASE_URI)
     Base.metadata.create_all(engine)
-    Base.metadata.bind = engine
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    dummy_sensor = Sensor(name=datetime.datetime.now().isoformat(' '))
-    session.add(dummy_sensor)
-    session.commit()
 
 
 if __name__ == '__main__':
